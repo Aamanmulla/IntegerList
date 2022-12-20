7351663043b8e4385371ceb691436f3b68bd3893
Aaman Mulla <98902201+Aamanmulla@users.noreply.github.com>
2022-12-20 17:47:16 +0530

    Add files via upload
    Add solution for 1.1 of set 1 (Integer list)

---
+++ 1.1_Integer_List.py
@@ -0,0 +1,17 @@
+lst=[]
+
+n=int(input("Enter the numbers in a list: "))
+for i in range(0, n):
+    ele = int(input())
+  
+    lst.append(ele)
+print("\nInput: ",lst)
+
+result1=lst.count(19)
+result2=lst.count(5)
+
+
+if result1==2 and result2>=3:
+    print("\nOutput: \nTrue\n")
+else:
+    print("\nOutput: \nFalse\n")
